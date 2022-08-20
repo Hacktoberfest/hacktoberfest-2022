8c14160e5b10b78b29cacc7332d8805014f0d0ef
MattIPv4 <me@mattcowley.co.uk>
2022-08-20 20:02:09 +0100

    Rough events content

---
+++ src/lib/events.js
@@ -0,0 +1,112 @@
+// TODO: Integrate with Airtable API
+export const fetchEvents = async () => [
+    {
+        type: [
+            'Premium Partner',
+            'Virtual',
+        ],
+        title: 'AppWrite Hacktoberfest Kickoff #1',
+        location: 'Phoenix, Arizona',
+        date: '9/3',
+        time: '7:30 AM UTC+05:30',
+        format: [
+            'Discord',
+            'YouTube',
+            'Hackathon',
+            'Video Call',
+        ],
+    },
+    {
+        type: [
+            'Community',
+            'In Person',
+        ],
+        title: 'Hacktoberfest SCE',
+        location: 'Bengaluru, India',
+        date: '10/2',
+        time: '12:30 PM UTC+05:30',
+        format: [
+            'Video Call',
+        ],
+    },
+    {
+        type: [
+            'DigitalOcean',
+            'Virtual',
+        ],
+        title: 'Hacktoberfest 2022',
+        location: 'New York, New York',
+        date: '10/1',
+        time: '12:00 PM UTC+05:30',
+        format: [
+            'Live Stream',
+        ],
+    },
+    {
+        type: [
+            'Premium Partner',
+            'Virtual',
+        ],
+        title: 'AppWrite Hacktoberfest Kickoff #1',
+        location: 'Phoenix, Arizona',
+        date: '9/3',
+        time: '7:30 AM UTC+05:30',
+        format: [
+            'Discord',
+            'YouTube',
+            'Hackathon',
+            'Video Call',
+        ],
+    },
+    {
+        type: [
+            'Community',
+            'In Person',
+        ],
+        title: 'Hacktoberfest SCE',
+        location: 'Bengaluru, India',
+        date: '10/2',
+        time: '12:30 PM UTC+05:30',
+        format: [
+            'Video Call',
+        ],
+    },
+    {
+        type: [
+            'DigitalOcean',
+            'Virtual',
+        ],
+        title: 'Hacktoberfest 2022',
+        location: 'New York, New York',
+        date: '10/1',
+        time: '12:00 PM UTC+05:30',
+        format: [
+            'Live Stream',
+        ],
+    },
+];
+
+// TODO: Integrate with Airtable API
+export const fetchSpeakers = async () => [
+    {
+        name: 'Chris Sevilla',
+        pronouns: 'he/him',
+        location: 'Las Vegas, NV, USA',
+        company: 'DigitalOcean',
+        social: '@Chris__Sev',
+    },
+    {
+        name: 'Rizel Scarlett',
+        pronouns: 'she/her',
+        location: 'Boston, MA, USA',
+        company: 'GitHub',
+        social: '@BlackGirlBytes',
+    },
+    {
+        name: 'Chris Sevilla',
+        pronouns: 'he/him',
+        location: 'Las Vegas, NV, USA',
+        company: 'DigitalOcean',
+        social: '@Chris__Sev',
+    },
+];
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,5 +1,108 @@
-const Events = () => {
-  return <h1>Events</h1>;
+import { fetchEvents, fetchSpeakers } from 'lib/events';
+import { useMemo, useState } from 'react';
+
+const Events = ({ events, speakers }) => {
+    const [ eventsCount, setEventsCount ] = useState(3);
+    const eventsList = useMemo(() => events.slice(0, eventsCount), [ events, eventsCount ]);
+
+    return (
+        <>
+            <h1>Events for 2022</h1>
+
+            <div>
+                <h2>Global Events</h2>
+                <p>
+                    Hacktoberfest events are happening all month long so you can join your friends day or night, from
+                    dusk to dawn, as you work to complete your pull/merge requests.
+                </p>
+
+                <div>
+                    { eventsList.map(event => (
+                        <div key={ event.title }>
+                            <p>{ event.type.map(type => `[ ${type} ]`).join(' ') }</p>
+                            <h3>{ event.title }</h3>
+                            <ul>
+                                <li>Location: { event.location }</li>
+                                <li>Date: { event.date }</li>
+                                <li>Time: { event.time }</li>
+                                <li>Format: { event.format.join(', ') }</li>
+                            </ul>
+                        </div>
+                    )) }
+                </div>
+
+                { eventsCount < events.length &&
+                    <button onClick={() => setEventsCount(count => count + 3)}>Load More Events</button> }
+            </div>
+
+            <div>
+                <h2>Event Organizers</h2>
+                <p>
+                    This kit gives you all that you need to host a successful Hacktoberfest event. We encourage virtual
+                    events and have included a collection of tips and tricks that will help you keep participants
+                    engaged.
+                </p>
+                <a href="">Download the Kit</a>
+
+                <div>
+                    <h3>Create an Online Meet Up</h3>
+                </div>
+
+                <div>
+                    <h3>Video Platforms &amp; Resources for Online Meetups</h3>
+                </div>
+
+                <div>
+                    <h3>Promoting Your Event</h3>
+                </div>
+            </div>
+
+            <div>
+                <h2>Speakers &amp; Facilitators</h2>
+                <p>
+                    This needs copy. Whether it’s your first time —or your ninth, it’s almost time to hack out four
+                    pristine pull/merge requests and complete your mission for open source. Join other members of the
+                    open source community on the Hacktoberfest Discord.
+                </p>
+
+                <div>
+                    { speakers.map(speaker => (
+                        <div key={speaker.name}>
+                            <h3>{speaker.name}</h3>
+                            <ul>
+                                <li>Pronouns: {speaker.pronouns}</li>
+                                <li>Location: {speaker.location}</li>
+                                <li>Company: {speaker.company}</li>
+                                <li>Social: {speaker.social}</li>
+                            </ul>
+                        </div>
+                    )) }
+                </div>
+            </div>
+
+            <div>
+                <h2>Brand Guidelines</h2>
+                <p>
+                    This Needs copy. kit gives you all that you need to host a successful Hacktoberfest event! We
+                    encourage virtual events and have included tips and tricks to keep participants engaged
+                    virtually.
+                </p>
+                <a href="">Download the Hacktoberfest Brand Guidelines</a>
+            </div>
+        </>
+    );
+};
+
+export const getStaticProps = async () => {
+    const events = await fetchEvents();
+    const speakers = await fetchSpeakers();
+
+    return {
+        props: {
+            events,
+            speakers,
+        },
+    };
 };
 
 export default Events;
