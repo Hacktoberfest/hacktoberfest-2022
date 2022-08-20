8e52233b6290903ab51a08a0c6834c45d34e069b
MattIPv4 <me@mattcowley.co.uk>
2022-08-20 21:14:33 +0100

    Rough about content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,47 @@
 const About = () => {
-  return <h1>About</h1>;
+    return (
+        <>
+            <h1>About</h1>
+
+            <div>
+                <h2>Hacktoberfest Lore</h2>
+                <p>
+                    Hacktoberfest is DigialOcean’s annual event that encourages people to contribute to open source
+                    throughout October. Much of modern tech infrastructure—including some of DigitalOcean’s own
+                    products—relies on open-source projects built and maintained by passionate people who often don’t
+                    have the staff or budgets to do much more than keep the project alive. Hacktoberfest is all about
+                    giving back to those projects, sharpening skills, and celebrating all things open source, especially
+                    the people that make open source so special.
+                </p>
+                <p>
+                    For the past 9 years, thousands of people—coders and non-coders alike—have participated in
+                    Hacktoberfest to support the projects they use and love, learn and practice skills that will enhance
+                    their careers, and meet new people who love open source as much as they do.
+                </p>
+            </div>
+
+            <div>
+                <h2>Advisory Council</h2>
+                <p>
+                    Each year, we bring together a talented group of open-source superfans who help ensure that
+                    Hacktoberfest is accessible, inclusive, and enriching for both contributors and maintainers.
+                </p>
+            </div>
+
+            <div>
+                <h2>New for 2022</h2>
+            </div>
+
+            <div>
+                <h3>Non-Code Contributions</h3>
+                <p>
+                    Hacktoberfest isn’t all about code. Keyboard jockeys who write, design, test, mentor, or organize
+                    all offer much needed support for open source projects all over the world.
+                </p>
+                <a href="">Learn About Non-Code Contributions</a>
+            </div>
+        </>
+    );
 };
 
 export default About;
