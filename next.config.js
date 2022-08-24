966463bc4638ef2349e4aba137556b8c12933676
MattIPv4 <me@mattcowley.co.uk>
2022-08-24 12:32:09 +0100

    Use consistent build ID for Next.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,31 @@
+const childProcess = require('child_process');
+const nextLog = require('next/dist/build/output/log');
+
+const getBuildId = () => {
+  // Prefer BUILD_ID env var if available
+  if (process.env.BUILD_ID !== undefined) return process.env.BUILD_ID;
+
+  // Attempt to get current git sha
+  try {
+    return childProcess.execSync('git rev-parse HEAD').toString().trim();
+  } catch (_) {
+    // Ignore any error
+  }
+
+  // Default to Next's own generation
+  return null;
+};
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  generateBuildId() {
+    const proposedBuildId = getBuildId();
+    nextLog.info(`Using build ID: ${proposedBuildId === null
+        ? 'auto-generated'
+        : `'${proposedBuildId}'`}`);
+    return proposedBuildId;
+  },
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
-}
-
-module.exports = nextConfig
+};
