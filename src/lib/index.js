d2491ba91d1c1b4fa0ba2ce4d8c23a63dc3d0f77
MattIPv4 <me@mattcowley.co.uk>
2022-08-31 01:04:50 +0100

    Update index page content

---
+++ src/lib/index.js
@@ -0,0 +1,30 @@
+export const events = [
+  {
+    title: 'Hacktoberfest Kick-Off',
+    content: 'The official beginning of Hacktoberfest. Join DigitalOcean developers as we begin the month-long hacking event you’ve been waiting for.',
+    date: 'Oct 04',
+    time: '11:00 AM Eastern Daylight Time',
+    location: 'Livestream'
+  },
+  {
+    title: 'Hacktoberfest Hack Party Asia Pacific Edition',
+    content: 'Get started with non-code contributions to Hacktoberfest no matter your experience level.',
+    date: 'Oct 12',
+    time: '11:00 AM Indian Standard Time',
+    location: 'Livestream'
+  },
+  {
+    title: 'Hacktoberfest Hack Party',
+    content: '',
+    date: 'Oct 20',
+    time: '',
+    location: 'Livestream'
+  },
+  {
+    title: 'Hacktoberfest Wrap Party',
+    content: '',
+    date: 'Oct 27',
+    time: '11:00 AM Eastern Daylight Time',
+    location: 'Livestream'
+  },
+];
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,84 +1,84 @@
 import Link from 'next/link';
+import Button from 'components/button';
+import { events } from 'lib';
 
 const Home = () => {
-    return (
-        <>
-            <h1>Registration Begins<br/>September 26</h1>
+  return (
+    <>
+      <h1>Registration Begins<br />September 26</h1>
 
-            <div>
-                <h2>Prepare to Hack</h2>
-                <p>
-                    Hacktoberfest is for everyone.
-                    Whether it’s your first time —or your ninth, it’s almost time to hack out four pristine pull/merge requests and complete your mission for open source.
-                    Join other members of the open source community on the Hacktoberfest Discord.
-                </p>
-                <a href="https://discord.gg/hacktoberfest">Join the Hacktoberfest Discord</a>
-            </div>
+      <div>
+        <h2>Prepare to Hack</h2>
+        <p>
+          Hacktoberfest is for everyone.
+          Whether it’s your first time—or your ninth—it’s almost time to hack out four pristine pull/merge requests and
+          complete your mission for open source.
+          Join other members of the open-source community on the Hacktoberfest Discord.
+        </p>
+        <Button special as="a" href="https://discord.gg/hacktoberfest">Join the Hacktoberfest Discord</Button>
+      </div>
 
-            <div>
-                <h3>Preptember</h3>
-                <p>
-                    September is the perfect time to prepare for Hacktoberfest.
-                    Jump start by choosing projects to contribute to, helping maintainers add the ‘hacktoberfest’ tag to their projects, or familiarizing yourself with Git.
-                </p>
-                <Link href="/events#kit">Get the Event Kit</Link>
-                <Link href="/participation">View Resources</Link>
-            </div>
+      <div>
+        <h3>Preptember</h3>
+        <p>
+          September is the perfect time to prepare for Hacktoberfest.
+          Get a jump start by finding projects to contribute to, adding the ‘hacktoberfest’ tag to your projects, or familiarizing yourself with Git.
+        </p>
+        <Link href="/events#kit" passHref>
+          <Button as="a" color="giga">Get the Event Kit</Button>
+        </Link>
+        <Link href="/participation" passHref>
+          <Button as="a" color="giga">How to Participate</Button>
+        </Link>
+      </div>
 
-            <div>
-                <h3>New for 2022</h3>
-                <p>
-                    Hacktoberfest isn’t all about code.
-                    Keyboard jockeys who write, design, test, mentor, or organize all offer much needed support for open-source projects all over the world.
-                </p>
-                <Link href="/about#non-code">Learn About Non-Code Contributions</Link>
-            </div>
+      <div>
+        <h3>New for 2022</h3>
+        <p>
+          Hacktoberfest isn’t all about code.
+          Anyone who writes, designs, tests, mentors, or organizes offers much needed support for open-source projects all over the world.
+        </p>
+        <Link href="/about#non-code" passHref>
+          <Button as="a" color="giga">Learn About Non-Code Contributions</Button>
+        </Link>
+      </div>
 
-            <div>
-                <h2>Events All Month Long</h2>
-                <p>
-                    It’s dangerous to code alone.
-                    Join forces in virtual and in-person events to get your pull/merge requests done as a team, learn new skills, and meet lifelong friends.
-                </p>
-                <Link href="/events">View &amp; Organize Events</Link>
-            </div>
+      <div>
+        <h2>Events All Month Long</h2>
+        <p>
+          Join forces in virtual and in-person events to get your pull/merge requests done as a team, learn new skills, and meet lifelong friends.
+        </p>
 
-            <div>
-                <h3>Hacktoberfest Kick-Off</h3>
-                <p>
-                    The official beginning of Hacktoberfest.
-                    Join DigitalOcean developers as we begin the month-long hacking event you’ve been waiting for.
-                </p>
-                <ul>
-                    <li>Date: September 29</li>
-                    <li>Time: 00:00</li>
-                    <li>Location: Online</li>
-                </ul>
-            </div>
+        {events.map(event => (
+          <div key={event.title}>
+            <h3>{event.title}</h3>
+            <p>{event.content}</p>
+            <p>Date: {event.date}</p>
+            <p>Time: {event.time}</p>
+            <p>Location: {event.location}</p>
+          </div>
+        ))}
 
-            <div>
-                <h3>H-Fest Hack Party</h3>
-                <p>
-                    Get started with non-code contributions to Hacktoberfest no matter your experience level.
-                </p>
-                <ul>
-                    <li>Date: October 5</li>
-                    <li>Time: 17:30-19:30 EST</li>
-                    <li>Location: WeWork NY</li>
-                </ul>
-            </div>
+        <Link href="/events" passHref>
+          <Button special as="a">See All Events</Button>
+        </Link>
+      </div>
 
-            <div>
-                <h2>Support Open Source</h2>
-                <p>
-                    Hacktoberfest is for everyone.
-                    Whether it’s your first time —or your ninth, it’s almost time to hack out four pristine pull/merge requests and complete your mission for open source.
-                    Join other members of the open source community on the Hacktoberfest Discord.
-                </p>
-                <Link href="/donate">Donate to Projects in Need</Link>
-            </div>
-        </>
-    );
+      <div>
+        <h2>Support Open Source</h2>
+        <p>
+          Open-source projects, maintained by community-minded coders, make the modern internet function.
+          Supporting that essential work, and the folks behind it, is what Hacktoberfest is all about.
+        </p>
+        <p>
+          You have skills that can help keep these projects continue running—let’s get to it.
+        </p>
+        <Link href="/donate">
+          <Button special as="a">Donate To Open-Source Projects</Button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Home;
