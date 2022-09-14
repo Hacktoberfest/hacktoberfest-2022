480bf0ffacb7441e845d804a66570ef6ba2e622b
MattIPv4 <me@mattcowley.co.uk>
2022-09-14 12:42:03 +0100

    Abstract the metadata fields for later use on profile

--- src/pages/register.js
+++ src/pages/register.js
@@ -6,6 +6,7 @@ import Divider from 'components/divider';
 import Section from 'components/section';
 import Button from 'components/button';
 import Loader from 'components/loader';
+import MetadataFields from 'components/profile/metadata-fields';
 
 import useAuth from 'hooks/useAuth';
 
@@ -17,24 +18,13 @@ const Register = () => {
   // Track the data we need to render
   const [ loaded, setLoaded ] = useState(false);
   const [ emails, setEmails ] = useState([]);
-  const [ metadata, setMetadata ] = useState({});
+  const [ metadata, setMetadata ] = useState([]);
 
   // Track the data the user enters
   const [ data, setData ] = useState({
     email: null,
     metadata: {},
   });
-  const updateMetadata = useCallback(obj => {
-    setData(prev => ({ ...prev, metadata: { ...prev.metadata, ...obj } }));
-  }, []);
-
-  // Provide a single checkbox to opt out of all marketing
-  const [ marketingDisabled, setMarketingDisabled ] = useState(false);
-  useEffect(() => {
-    if (marketingDisabled && metadata.marketing) {
-      updateMetadata(metadata.marketing.reduce((obj, item) => ({ ...obj, [item.name]: false }), {}));
-    }
-  }, [ marketingDisabled, metadata, updateMetadata ]);
 
   // Once initial auth has completed, load the data we need to render
   useEffect(() => {
@@ -48,37 +38,24 @@ const Register = () => {
 
       // Fetch all the metadata for the event
       const rawMetadata = await fetchMetadata(auth.token);
+      setMetadata(rawMetadata);
 
       // Store default values for each metadata item
-      updateMetadata(rawMetadata.reduce((obj, item) => ({
-        ...obj,
-        [item.name]: item.datatype === 'boolean' ? false : (item.datatype === 'string' ? '' : null),
-      }), {}));
-
-      // Group the metadata by category for the UI
-      const groupedMetadata = rawMetadata.reduce((obj, item) => ({
-        ...obj,
-        [item.name.split('-')[0]]: [
-          ...(obj[item.name.split('-')[0]] || []),
-          {
-            position: Number(item.message?.match(/^(\d+): /)?.[1]) || 0,
-            item: {
-              ...item,
-              message: item.message?.replace(/^(\d+): /, ''),
-            },
-          },
-        ],
-      }), {});
-      for (const key in groupedMetadata) {
-        groupedMetadata[key].sort((a, b) => a.position - b.position);
-        groupedMetadata[key] = groupedMetadata[key].map(item => item.item);
-      }
-      setMetadata(groupedMetadata);
+      setData(prev => ({
+        ...prev,
+        metadata: {
+          ...prev.metadata,
+          ...rawMetadata.reduce((obj, item) => ({
+            ...obj,
+            [item.name]: item.datatype === 'boolean' ? false : (item.datatype === 'string' ? '' : null),
+          }), {}),
+        },
+      }));
 
       // Show the page
       setLoaded(true);
     })();
-  }, [ auth, loaded, updateMetadata ]);
+  }, [ auth, loaded ]);
 
   // Handle form submission
   const form = useRef();
@@ -114,151 +91,7 @@ const Register = () => {
           <p>Hello, {auth.user.name}</p>
 
           <form ref={form} onSubmit={submit}>
-            <fieldset>
-              <label>[ Self-identification ]</label>
-
-              <fieldset>
-                <label>Email</label>
-                <select
-                  name="email"
-                  value={data.email}
-                  onChange={e => setData(prev => ({ ...prev, email: e.target.value }))}
-                  required
-                >
-                  {emails.map(email => (
-                    <option key={email} value={email}>{email}</option>
-                  ))}
-                </select>
-              </fieldset>
-
-              <fieldset>
-                <label>How will you be participating? (select all that apply)</label>
-                {metadata.role?.map(meta => (
-                  <div key={meta.name}>
-                    <label>
-                      <input
-                        type="checkbox"
-                        name={meta.name}
-                        onChange={e => updateMetadata({ [meta.name]: e.target.checked })}
-                        checked={data.metadata[meta.name]}
-                      />
-                      {meta.title}
-                      <i>{meta.message}</i>
-                    </label>
-                  </div>
-                ))}
-              </fieldset>
-
-              <fieldset>
-                <label>What is your experience level?</label>
-                {metadata.stage?.map(meta => (
-                  <div key={meta.name}>
-                    <label>
-                      <input
-                        type="radio"
-                        name="experience"
-                        value={meta.name}
-                        onChange={e => updateMetadata(metadata.stage.reduce((obj, item) => ({
-                          ...obj,
-                          [item.name]: meta.name === item.name && e.target.checked,
-                        }), {}))}
-                      />
-                      {meta.title}
-                      <i>{meta.message}</i>
-                    </label>
-                  </div>
-                ))}
-              </fieldset>
-
-              <fieldset>
-                <label>How would you like to contribute? (select all that apply)</label>
-                {metadata.type?.map(meta => (
-                  <div key={meta.name}>
-                    <label>
-                      <input
-                        type="checkbox"
-                        name={meta.name}
-                        onChange={e => updateMetadata({ [meta.name]: e.target.checked })}
-                        checked={data.metadata[meta.name]}
-                      />
-                      {meta.title}
-                      <i>{meta.message}</i>
-                    </label>
-                  </div>
-                ))}
-              </fieldset>
-            </fieldset>
-
-            <fieldset>
-              <label>[ Operational opt-ins ]</label>
-
-              {metadata.operational?.map(meta => (
-                <div key={meta.name}>
-                  <label>
-                    <input
-                      type="checkbox"
-                      name={meta.name}
-                      onChange={e => updateMetadata({ [meta.name]: e.target.checked })}
-                      checked={data.metadata[meta.name]}
-                    />
-                    {meta.title}
-                    <i>{meta.message}</i>
-                  </label>
-                </div>
-              ))}
-            </fieldset>
-
-            <fieldset>
-              <label>[ Marketing opt-ins ]</label>
-
-              {metadata.marketing?.map(meta => (
-                <div key={meta.name}>
-                  <label>
-                    <input
-                      type="checkbox"
-                      name={meta.name}
-                      onChange={e => updateMetadata({ [meta.name]: e.target.checked })}
-                      checked={data.metadata[meta.name]}
-                      disabled={marketingDisabled}
-                    />
-                    {meta.title}
-                    <i>{meta.message}</i>
-                  </label>
-                </div>
-              ))}
-
-              <div>
-                <label>
-                  <input
-                    type="checkbox"
-                    name="marketing-disable"
-                    onChange={e => setMarketingDisabled(e.target.checked)}
-                    checked={marketingDisabled}
-                  />
-                  I do not wish to receive any marketing updates from Hacktoberfest’s partners.
-                </label>
-              </div>
-            </fieldset>
-
-            <fieldset>
-              <label>[ Rules &amp; terms ]</label>
-
-              {metadata.agree?.map(meta => (
-                <div key={meta.name}>
-                  <label>
-                    <input
-                      type="checkbox"
-                      name={meta.name}
-                      onChange={e => updateMetadata({ [meta.name]: e.target.checked })}
-                      checked={data.metadata[meta.name]}
-                      required
-                    />
-                    {meta.title}
-                    <i>{meta.message}</i>
-                  </label>
-                </div>
-              ))}
-            </fieldset>
+            <MetadataFields emails={emails} metadata={metadata} value={data} onChange={setData} />
           </form>
 
           <Button onClick={() => auth.reset()}>Logout</Button>
