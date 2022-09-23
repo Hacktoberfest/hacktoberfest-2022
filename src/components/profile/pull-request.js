e134840f8ab391bbfa7a69c287f268d57605d4fa
MattIPv4 <me@mattcowley.co.uk>
2022-09-23 19:33:29 +0100

    Show PR state on hover

--- src/components/profile/pull-request.js
+++ src/components/profile/pull-request.js
@@ -1,14 +1,50 @@
 import styled from 'styled-components';
 
+import { MarkdownInline } from '../markdown';
+
 import useCountdown from 'hooks/useCountdown';
 
 import { providerMap, prWaitingTime } from 'lib/config';
+import { pullRequestStates, pullRequestValidation } from 'lib/profile';
+
+const StyledState = styled.div`
+  display: none;
+  position: absolute;
+  top: 100%;
+  left: -1px;
+  right: -1px;
+  padding: 8px;
+  background: ${props => props.theme.body};
+  z-index: 1;
+  border-radius: 0 0 8px 8px;
+  border: solid ${props => props.theme.text};
+  border-width: 0 1px 1px 1px;
+`;
 
 const StyledPullRequest = styled.div`
-  margin: 16px 8px;
-  
-  p {
-    margin: 4px 0 !important;
+  > a {
+    margin: 8px 0;
+    padding: 8px;
+    border-radius: 8px 8px 0 0;
+    display: block;
+    position: relative;
+    min-width: 60rem;
+    max-width: 100%;
+    border: solid transparent;
+    border-width: 1px 1px 0 1px;
+    
+    &:hover {
+      background: ${props => props.theme.body};
+      border-color: ${props => props.theme.text};
+      
+      ${StyledState} {
+        display: block;
+      }
+    }
+    
+    p {
+      margin: 4px 0 !important;
+    }
   }
 `;
 
@@ -36,6 +72,11 @@ const PullRequest = ({ data, as }) => {
         <p>
           {data.title}
         </p>
+
+        <StyledState>
+          <MarkdownInline string={pullRequestStates[data.state.state]} />
+          <MarkdownInline string={pullRequestValidation} />
+        </StyledState>
       </a>
     </StyledPullRequest>
   );
