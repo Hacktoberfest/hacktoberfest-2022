5df77ae30ab5760c12751c984eb400ef16e36bc6
MattIPv4 <me@mattcowley.co.uk>
2022-09-24 02:37:58 +0100

    Add some color to PRs

--- src/components/profile/pull-request.js
+++ src/components/profile/pull-request.js
@@ -7,6 +7,21 @@ import useCountdown from 'hooks/useCountdown';
 import { providerMap, prWaitingTime } from 'lib/config';
 import { pullRequestStates, pullRequestValidation } from 'lib/profile';
 
+const stateColors = {
+  excluded: theme => theme.spark,
+  spam: theme => theme.spark,
+  'not-participating': theme => theme.surf,
+  invalid: theme => theme.spark,
+  'not-accepted': theme => theme.surf,
+  waiting: theme => theme.surf,
+  accepted: theme => theme.giga,
+};
+
+const providerColors = {
+  github: theme => theme.psybeam,
+  gitlab: theme => theme.spark,
+};
+
 const StyledState = styled.div`
   display: none;
   position: absolute;
@@ -14,12 +29,16 @@ const StyledState = styled.div`
   left: -1px;
   right: -1px;
   padding: 8px;
-  background: ${props => props.theme.body};
+  background: linear-gradient(to bottom, rgba(23, 15, 30, 0.8) 0%, rgba(23, 15, 30, 0.95) 100%);
   z-index: 1;
   border-radius: 0 0 8px 8px;
-  border: solid ${props => props.theme.text};
+  border: solid transparent;
   border-width: 0 1px 1px 1px;
 `;
+
+const StyledEyebrow = styled.p``;
+
+const StyledProvider = styled.span``;
 
 const StyledPullRequest = styled.div`
   > a {
@@ -34,16 +53,40 @@ const StyledPullRequest = styled.div`
     border-width: 1px 1px 0 1px;
     
     &:hover {
-      background: ${props => props.theme.body};
-      border-color: ${props => props.theme.text};
+      border-color: ${props => stateColors[props.state](props.theme)};
+      
+      &::before {
+        display: block;
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        box-shadow: ${props => stateColors[props.state](props.theme)} 0px 0px 12px;
+        clip-path: inset(-12px -12px 0 -12px);
+      }
       
       ${StyledState} {
         display: block;
+        border-color: ${props => stateColors[props.state](props.theme)};
+        box-shadow: ${props => stateColors[props.state](props.theme)} 0px 0px 12px;
+        clip-path: inset(0 -12px -12px -12px);
       }
     }
     
     p {
       margin: 4px 0 !important;
+    }
+    
+    ${StyledEyebrow} {
+      color: ${props => stateColors[props.state](props.theme)};
+      text-shadow: 0 0 3px ${props => stateColors[props.state](props.theme)};
+    }
+    
+    ${StyledProvider} {
+      color: ${props => providerColors[props.provider](props.theme)};
+      text-shadow: 0 0 3px ${props => providerColors[props.provider](props.theme)};
     }
   }
 `;
@@ -53,9 +96,9 @@ const PullRequest = ({ data, as }) => {
   const [ days, hours, minutes, seconds ] = useCountdown(new Date(data.state.timestamp || 0).getTime() + prWaitingTime);
 
   return (
-    <StyledPullRequest as={as}>
+    <StyledPullRequest as={as} state={data.state.state} provider={data.provider}>
       <a href={data.url} target="_blank" rel="noreferrer">
-        <p>
+        <StyledEyebrow>
           [ {data.state.state} ]
           {data.state.state === 'waiting' && (
             <>
@@ -63,12 +106,14 @@ const PullRequest = ({ data, as }) => {
               [ {days}:{hours}:{minutes}:{seconds} ]
             </>
           )}
-        </p>
+        </StyledEyebrow>
+
         <p>
-          [ {providerMap[data.provider]} ]
+          <StyledProvider>[ {providerMap[data.provider]} ]</StyledProvider>
           {' '}
           {data.target}#{data.number}
         </p>
+
         <p>
           {data.title}
         </p>
