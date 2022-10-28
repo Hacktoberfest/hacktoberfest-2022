dc050fc90fe3fe237bdd0859d6456a9530fdb475
MattIPv4 <me@mattcowley.co.uk>
2022-10-28 17:04:56 +0100

    Do two purges on deploy to get past caching issues

--- src/build/post/cache.mjs
+++ src/build/post/cache.mjs
@@ -1,19 +1,6 @@
 import fetch from 'cross-fetch';
 
-const cache = async () => {
-  // Check we have the env vars we need
-  if (!process.env.CLOUDFLARE_API_TOKEN) {
-    console.warn('CLOUDFLARE_API_TOKEN not set, cannot clear Cloudflare cache');
-    return;
-  }
-  if (!process.env.CLOUDFLARE_ZONE_ID) {
-    console.warn('CLOUDFLARE_ZONE_ID not set, cannot clear Cloudflare cache');
-    return;
-  }
-
-  // Wait 30s to give the site time to deploy
-  await new Promise((resolve) => setTimeout(resolve, 30000));
-
+const purge = async () => {
   // Do the purge
   const resp = await fetch(
     `https://api.cloudflare.com/client/v4/zones/${encodeURIComponent(process.env.CLOUDFLARE_ZONE_ID)}/purge_cache`,
@@ -33,6 +20,26 @@ const cache = async () => {
   const body = await resp.text().catch(() => '');
   if (!resp.ok) throw new Error(`Cloudflare API error: ${resp.status} ${resp.statusText}: ${body}`);
   console.log(`Cloudflare API response: ${resp.status} ${resp.statusText}: ${body}`);
+};
+
+const cache = async () => {
+  // Check we have the env vars we need
+  if (!process.env.CLOUDFLARE_API_TOKEN) {
+    console.warn('CLOUDFLARE_API_TOKEN not set, cannot clear Cloudflare cache');
+    return;
+  }
+  if (!process.env.CLOUDFLARE_ZONE_ID) {
+    console.warn('CLOUDFLARE_ZONE_ID not set, cannot clear Cloudflare cache');
+    return;
+  }
+
+  // Do a purge after waiting 30s to give the site time to deploy
+  await new Promise((resolve) => setTimeout(resolve, 30000));
+  await purge();
+
+  // Wait 30s for the initial purge to complete, and then purge again to be safe
+  await new Promise((resolve) => setTimeout(resolve, 30000));
+  await purge();
 };
 
 export default cache;
