41e04adc14614baf16001794a58dd973e1579141
MattIPv4 <mcowley@digitalocean.com>
2023-09-26 17:44:34 +0100

    Refactor typing animation to use JS

--- src/components/type.js
+++ src/components/type.js
@@ -1,60 +1,91 @@
-import { useMemo } from 'react';
-import { keyframes, css } from 'styled-components';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
-const typingAnim = () => keyframes`
-  from {
-    opacity: 0;
-  }
+const interval = 100;
+const idle = 1000;
 
-  to {
-    opacity: 1;
-  }
-`;
-
-const typingPseudoAnim = () => keyframes`
-  from {
-    content: '_';
-  }
-
-  to {
-    content: '';
-  }
-`;
+const srOnly = {
+  position: 'absolute',
+  width: '1px',
+  height: '1px',
+  margin: '-1px',
+  overflow: 'hidden',
+  clip: 'rect(0, 0, 0, 0)',
+};
 
 const Type = ({ text }) => {
-  const chars = useMemo(() => text.split(''), [text]);
+  const ref = useRef(null);
 
-  const styles = useMemo(() => css`
-    > span {
-      ${chars.map((_, i) => css`
-        &:nth-child(${i + 1}) {
-          > span {
-            opacity: 0;
-            animation: ${typingAnim} 0.5s ${i * 0.1}s forwards steps(1);
-          }
+  const [progress, setProgress] = useState(0);
+  const progressTimer = useRef(null);
 
-          &::after {
-            content: '';
-            animation: ${typingPseudoAnim} 0.1s ${(i * 0.1) + 0.5}s forwards steps(1);
-          }
+  const done = useMemo(() => progress === text.length, [progress, text.length]);
+
+  const [cursor, setCursor] = useState(true);
+  const cursorTimer = useRef(null);
+
+  // Start incrementing the progress when the element is in view
+  useEffect(() => {
+    if (done) return;
+
+    const elm = ref.current;
+    if (!elm) return;
+
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting && !progressTimer.current) {
+          progressTimer.current = setInterval(() => {
+            setProgress(current => current + 1);
+          }, interval);
         }
-      `)}
-    }
+      },
+      { threshold: 0.25 }
+    );
+    observer.observe(elm);
 
-    &::after {
-      content: '';
-      animation: ${typingPseudoAnim} 1s ${(chars.length * 0.1) + 0.5}s forwards steps(1);
+    return () => observer.unobserve(elm);
+  }, [done]);
+
+  // When we finish typing, start the idle timer
+  useEffect(() => {
+    if (done && cursor && !cursorTimer.current) {
+      if (progressTimer.current) {
+        clearInterval(progressTimer.current);
+        progressTimer.current = null;
+      }
+
+      cursorTimer.current = setTimeout(() => {
+        setCursor(false);
+        cursorTimer.current = null;
+      }, idle);
     }
-  `, [chars]);
+  }, [done, cursor]);
+
+  // Clean up any timers when the component unmounts
+  useEffect(() => {
+    return () => {
+      if (progressTimer.current) {
+        clearInterval(progressTimer.current);
+        progressTimer.current = null;
+      }
+
+      if (cursorTimer.current) {
+        clearTimeout(cursorTimer.current);
+        cursorTimer.current = null;
+      }
+    };
+  }, []);
 
   return (
-    <span css={styles}>
-      {chars.map((char, i) => (
-        <span key={i}>
-          <span>{char}</span>
-        </span>
-      ))}
-    </span>
+    <>
+      <span ref={ref} aria-hidden="true">
+        {text.slice(0, progress)}
+        {cursor && '_'}
+        {!done && <span style={{ opacity: 0 }}>{text.slice(progress)}</span>}
+      </span>
+
+      {/* Screen readers should read the full text */}
+      <span style={srOnly}>{text}</span>
+    </>
   );
 };
 
