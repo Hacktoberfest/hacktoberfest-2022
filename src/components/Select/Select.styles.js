3b6cf689d23d0d38ddd512e10d91d0831b7676cb
MattIPv4 <mcowley@digitalocean.com>
2023-09-26 18:09:59 +0100

    Fix background color on select for Windows

--- src/components/Select/Select.styles.js
+++ src/components/Select/Select.styles.js
@@ -1,11 +1,11 @@
-import styled, {keyframes} from 'styled-components';
+import styled from 'styled-components';
 import { breakpoints as bp, determineMediaQuery as mQ } from 'themes/breakpoints';
-import { body20, headline32 } from 'themes/typography';
+import { body20 } from 'themes/typography';
 
 export const StyledSelect = styled.div`
   position: relative;
 
-  &::after {
+  &::before {
     content: '';
     position: absolute;
     top: 50%;
@@ -14,23 +14,43 @@ export const StyledSelect = styled.div`
     background: url('data:image/svg+xml,<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.50563 10.2556C7.05481 9.70646 7.94519 9.70646 8.49437 10.2556L15 16.7613L21.5056 10.2556C22.0548 9.70646 22.9452 9.70646 23.4944 10.2556C24.0435 10.8048 24.0435 11.6952 23.4944 12.2444L15.9944 19.7444C15.4452 20.2935 14.5548 20.2935 14.0056 19.7444L6.50563 12.2444C5.95646 11.6952 5.95646 10.8048 6.50563 10.2556Z" fill="%23EFEDEF"/></svg>');
     width: 30px;
     height: 30px;
+    z-index: 1;
+    pointer-events: none;
+  }
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    background: ${({ theme }) => theme.card.bg};
+    top: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+  }
+
+  &::after,
+  select {
+    border-radius: 12px;
+    box-shadow: inset 0 0 0 1px ${({ theme }) => theme.colors.neutral.manga400};
+
+    ${mQ(bp.desktop)} {
+      border-radius: 16px;
+    }
   }
 
   select {
     ${body20};
     appearance: none;
-    background: ${({ theme }) => theme.card.bg};
+    background: ${({ theme }) => theme.colors.neutral.void200};
     backdrop-filter: blur(5px);
     border: 0;
-    border-radius: 12px;
-    box-shadow: inset 0 0 0 1px ${({ theme }) => theme.colors.neutral.manga400};
     color: ${({ theme }) => theme.colors.neutral.manga200};
     margin: 0;
     padding: 12px 78px 12px 24px;
     width: 100%;
 
     ${mQ(bp.desktop)} {
-      border-radius: 16px;
       padding: 16px 78px 16px 24px;
     }
 
