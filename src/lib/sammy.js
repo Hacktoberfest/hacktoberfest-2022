e7695dadcce6038388679d6863639e26042e2527
MattIPv4 <me@mattcowley.co.uk>
2024-09-25 18:07:35 +0100

    Add Nohemi to Sammy easter egg

--- src/lib/sammy.js
+++ src/lib/sammy.js
@@ -37,7 +37,7 @@ export const sammy = `
   /*******   //*******    /********//******   //**   //******
   ///////     ///////     ////////  //////     //     //////
 
-@eye_of_doritto, @erhilse and @MattIPv4 were here.
+@nohemidesign, @eye_of_doritto, @erhilse and @MattIPv4 were here.
 We hope you enjoy the theme for Hacktoberfest this year!
 
 (Oh, btw, we're hiring: https://www.digitalocean.com/careers)
