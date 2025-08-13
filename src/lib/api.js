5c344398ca63e425bc98e78daf1668379c05ccc0
rdalal-do <rdalal@digitalocean.com>
2025-08-14 04:24:22 +0530

    createExcludedRepository API Update Data

--- src/lib/api.js
+++ src/lib/api.js
@@ -164,6 +164,8 @@ export const createExcludedRepository = async (userId, token, data) =>
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
+      body: JSON.stringify({
+        repos: data,
+      }),
     },
   );
