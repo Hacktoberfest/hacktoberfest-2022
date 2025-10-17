62bdcf057256bd51ae7a59dbc9074b08dcdfbe0a
Molnar Szabolcs <72910308+szabi-heits@users.noreply.github.com>
2025-10-17 10:07:30 +0300

    Update robots txt (#176)
    * Disallow /auth in robots.txt
    
    * Add the path to the correct location
    
    * Complete the sitemap
    
    * Add trailing slash

--- src/build/robots.mjs
+++ src/build/robots.mjs
@@ -5,7 +5,7 @@ const BASE_URL = (process.env.BASE_URL || '').replace(/\/*$/, '');
 const robots = async () => {
   await writeFile(
     new URL('../../public/robots.txt', import.meta.url),
-    `User-agent: *\nDisallow:\nSitemap: ${BASE_URL}/sitemap.xml`,
+    `User-agent: *\nDisallow: /auth/\nSitemap: ${BASE_URL}/sitemap.xml`,
   );
 };
 
